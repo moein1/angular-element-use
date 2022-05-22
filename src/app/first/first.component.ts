75189bf87b7dc1a79982b2627a349648e4d2ff9f
moein1 <karimi.mohammad.main@gmail.com>
2022-05-22 14:52:19 +1000

    some more changes.

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit , ElementRef ,ViewChild} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-first',
   templateUrl: './first.component.html',
   styleUrls: ['./first.component.scss']
 })
+
 export class FirstComponent implements OnInit {
-  
   constructor() { }
     ngOnInit(): void {
       const button = document.querySelector('app-button');
       button?.addEventListener('action', (event: any)=>{
         console.log(`action created , ${event.detail} ` )
       })
+      setTimeout(() => {
+        (button as any).label = 'second label';
+    }, 4000);
   }
   
 
